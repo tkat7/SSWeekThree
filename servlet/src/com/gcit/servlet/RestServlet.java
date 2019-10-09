36527ec5f646fce3ddd396f446cff45ee48613f2
tyler <tkatona2013@gmail.com>
2019-10-09 16:51:55 -0400

    Created project

---
+++ servlet/src/com/gcit/servlet/RestServlet.java
@@ -0,0 +1,53 @@
+package com.gcit.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RestServlet
+ */
+@WebServlet({"/user", "/user/", "/user/id/*"})
+public class RestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    
+    @Override
+    public void init() {
+    	System.out.println("**************************************");
+    	System.out.println("**************************************");
+    	System.out.println("**************************************");
+    	System.out.println("***The Servlet is being initialized***");
+    	System.out.println("**************************************");
+    	System.out.println("**************************************");
+    	System.out.println("**************************************");
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
